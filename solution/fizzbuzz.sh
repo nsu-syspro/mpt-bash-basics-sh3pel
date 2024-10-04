f94afeff3d38953d8a2f2b31fac358fbb4557150
Vladislav <sh3pelvlad@gmail.com>
2024-10-04 23:25:17 +0700

    Solution 4 task

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -1,7 +1,7 @@
 n="$1"
 
 for ((i=1;i<=n;i++)); do
-    if ! ((i%15)); then echo FizzBuzz
+    if ! ((i%15)); then echo Fizz Buzz
     elif ! ((i%5)); then echo Buzz
     elif ! ((i%3)); then echo Fizz
     else echo $i; fi;
