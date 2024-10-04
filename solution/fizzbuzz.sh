93ed1dc63c07711d50af3ee8e45f83833f128c2f
Vladislav <sh3pelvlad@gmail.com>
2024-10-04 23:28:39 +0700

    Solution 4 task new100

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -1,8 +1,8 @@
 n="$1"
 
 for ((i=1;i<=n;i++)); do
-    if ! ((i%15)); then echo Fizz Buzz
-    elif ! ((i%5)); then echo Buzz
-    elif ! ((i%3)); then echo Fizz
+    if ! ((i%15)); then echo "Fizz Buzz"
+    elif ! ((i%5)); then echo "Buzz"
+    elif ! ((i%3)); then echo "Fizz"
     else echo $i; fi;
 done
